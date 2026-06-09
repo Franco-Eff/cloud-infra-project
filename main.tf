260d1ebfae9a139d9a9fcdac1a3e0fc6110573cc
Franco-Eff <francorosito@yahoo.com>
2026-06-09 11:13:20 -0400

    code fixing

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Creates the main VPC that contains all project networking resources 
 resource "aws_s3_bucket" "main" {
   bucket = "${var.project_name}-storage-bucket"
 
--- providers.tf
+++ providers.tf
@@ -1,20 +1,21 @@
+# Configures Terraform to use the AWS provider and run remotely via Terraform Cloud
 terraform {
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 
-    cloud {
-        organization = "francorosito-org"
-        workspaces {
-            name = "cloud-infra-dev"
-        }
+  cloud {
+    organization = "francorosito-org"
+    workspaces {
+      name = "cloud-infra-dev"
     }
+  }
 }
 
+# Sets the AWS region using the variable defined in variables.tf
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
-
